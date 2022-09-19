ba38dfe13acdde35e62d348a3082b7fa6e99e1ce
kmalamin <alaminkhanstd@gmail.com>
2022-09-19 20:38:44 +0600

    add protfolio item

--- assets/js/function.js
+++ assets/js/function.js
@@ -174,7 +174,7 @@ function showPortfolioInUi(elem){
 }
 
 async function gettingPortfolioItems(){
-    const res = await fetch('https://json.extendsclass.com/bin/b1f487515f49')
+    const res = await fetch('https://json.extendsclass.com/bin/a808820b8c41')
     const result = await res.json()
     
     return result
